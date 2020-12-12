0cdf6aa55ff22414c0d810cd31e5b0d676e56088
Armand <armanddfl@gmail.com>
2020-12-12 15:06:03 +0100

    Sign in finish

--- main.c
+++ main.c
@@ -162,9 +162,67 @@ int main(int argc, char **argv)
 
 int SignIn(){
 
-    printf("\nSign in ok\n");
+    char pseudo[50];
+    char password[100];
+    char query[255];
+    int check=1;
+    MYSQL_RES *result=NULL;
+    MYSQL_ROW row;
+    int id;
 
-    return 0;
+    MYSQL mysql;
+    mysql_init(&mysql);
+    mysql_options(&mysql,MYSQL_READ_DEFAULT_GROUP,"option");
+
+    if(mysql_real_connect(&mysql,"localhost","root","root","picomancer",0,NULL,0)){
+
+        printf("\nSign in:\n");
+
+        do{
+           check=1;
+           printf("Enter your pseudo:\n\n");
+           fflush(stdin);
+           fgets(pseudo,50,stdin);
+           if(pseudo[strlen(pseudo)-1]=='\n'){
+               pseudo[strlen(pseudo)-1]='\0';
+           }
+
+            printf("Enter your password:\n\n");
+            fflush(stdin);
+            fgets(password,100,stdin);
+            if(password[strlen(password)-1]=='\n'){
+                password[strlen(password)-1]='\0';
+            }
+
+            strcpy(query,"SELECT id,password FROM USER WHERE pseudo='");
+            strcat(query,pseudo);
+            strcat(query,"'");
+
+            mysql_query(&mysql,query);
+            result = mysql_store_result(&mysql);
+            row = mysql_fetch_row(result);
+
+            if(!row){
+                printf("Incorrect pseudo");
+                check=0;
+            }
+
+            if(check!=0){
+                if(strstr(password,row[1])==NULL){
+                    printf("Incorrect password");
+                    check=0;
+                }
+            }
+
+        }while(check!=1);
+
+        printf("You are connected");
+        sscanf(row[0],"%d",&id);
+
+    }else{
+        printf("ERROR: An error occurred while connecting to the DB!");
+    }
+    return id;
 
 }
 
@@ -187,7 +245,7 @@ int SignUp(){
 
     if(mysql_real_connect(&mysql,"localhost","root","root","picomancer",0,NULL,0)){
 
-        printf("\nSign in:\n");
+        printf("\nSign up:\n");
 
         do{
             check=1;
@@ -313,9 +371,6 @@ int SignUp(){
         strcat(query,password);
         strcat(query,"')");
 
-
-        printf("\n%s",query);
-
         //sprintf(query,"INSERT INTO USER (pseudo,mail,city,password) VALUES('%s','%s','%s','%s');",pseudo,mail,city,password);
 
         mysql_query(&mysql,query);
@@ -330,9 +385,9 @@ int SignUp(){
 
         if(row){
             sscanf(row[0],"%d",&id);
-            printf("Welcome %s, your account has been successfully created",pseudo);
+            printf("\nWelcome %s, your account has been successfully created\n",pseudo);
         }else{
-            printf("ERROR: Your account can't be create");
+            printf("\nERROR: Your account can't be create\n");
         }
 
         //mysql_query(&mysql,"INSERT INTO USER (pseudo,mail,city,password) VALUES('Izonite','armanddfl@gmail.com','Chatou','Test1234')");
