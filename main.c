620ab14ec3b55c697e34c58121e05eced74aa440
Theo <theojherve@gmail.com>
2020-12-25 15:20:52 +0100

    functionality clean with bugs CURL and Read file
    not working ATM

--- main.c
+++ main.c
@@ -63,8 +63,14 @@ int main(int argc, char **argv)
     return 1;
 } */
 
+/*
+Programmed by HERVÉ Théo and DE FARIA LEITE Armand
+December 2020
+Display Paris's bars (retrieve Api information via Curl on a file, sort the information we need and put them in database)
+*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <curl.h>
 
 void getApiViaCurl(FILE *fp){
@@ -86,6 +92,8 @@ void getApiViaCurl(FILE *fp){
         errbuf[0] = 0;
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
 
+        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);//enable progress meter
+
         result = curl_easy_perform(curl); //return if the dl was successful (might take few seconds)
 
         if(result != CURLE_OK) {
@@ -105,7 +113,24 @@ void getApiViaCurl(FILE *fp){
 
 }
 
-void compareFile(FILE *fp){
+char *getData(FILE *fp, char *data){
+	char buffer;
+	int i;
+
+	while(fread(&buffer, sizeof(char), 1, fp)){
+		if(buffer == '"' || buffer == '['){
+			int i = 0;
+			while(fread(&buffer, sizeof(char), 1, fp) && buffer != '"' && buffer != ']'){
+				data[i] = buffer;
+				i++;
+			}
+			data[i] = '\0';
+			return data;
+		}
+	}
+}
+
+/*void compareFile(FILE *fp){
 
     FILE *fp2;
 
@@ -117,11 +142,57 @@ void compareFile(FILE *fp){
     fp2 = fopen("testCurlApiExport/script1.json", "rb");    //open the new downloaded file
     if(fp2 != NULL){
         fseek(fp, 0, SEEK_END);
-        position = ftell(fp)
+        position = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+
     }
+
+}*/
+
+void readFile(FILE *fp){
+
+    char buffer;
+	char *key;
+	char *data;
+	int i;			// Permet de naviguer dans la variable key pour construire le mot clé
+	int iData;		// Stock un nombre qui correspond au nombre de champ récupérer
+
+	iData = 0;
+
+	key = malloc(sizeof(char) * 255);
+	data = malloc(sizeof(char) * 255);
+
+	// Ouvre le fichier "script1.json" en lecture bit
+	fp = fopen("script1.json", "rb");
+
+	while(fread(&buffer, sizeof(char), 1, fp)){// Lit un caractère qui sera stocké dans un buffer puis fait avancer le curseur de lecture
+		if(buffer == '"'){ // => Début de construction du mot clé
+			i = 0;
+			while(fread(&buffer, sizeof(char), 1, fp) && buffer != '"'){ // => Continue la construction du mot clé tant que le buffer ne lit pas '"'
+				key[i] = buffer;
+				i++;
+			}
+			key[i] = '\0';
+
+			if(!strcmp(key, "geo_point_2d") || !strcmp(key, "date_periode") || !strcmp(key, "lieu1") || !strcmp(key, "libelle_type")) // Vérifie si le mot key est identique à un des trois mots clés choisis
+				data = getData(fp, data); // Appel de la fonction getData
+
+			printf("%s = %s\n", key, data);
+
+			iData++;			//
+								// Permet de faire un saut de
+			if(iData % 3 == 0)	// ligne tout les 3 champs lus
+				printf("\n");	// (juste pour la lisibilité dans la console)
+		}
+	}
 
 }
 
+
+/*
+	Le programme a pour but d'afficher les différentes champs d'informations de chaque restaurant
+	écrit dans un fichier.
+*/
 int main(int argc, char **argv){
 
     FILE *fp;
@@ -129,6 +200,8 @@ int main(int argc, char **argv){
     getApiViaCurl(fp);
 
     //compareFile(fp);
+
+    readFile(fp);
 
     return 0;
 
