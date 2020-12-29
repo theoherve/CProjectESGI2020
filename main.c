499cfaac820e238b3c2cbcc32b10658dc690e6c6
Theo <theojherve@gmail.com>
2020-12-29 20:25:23 +0100

    End of menu selections (selectFromCategory and researchFromAddress)

--- main.c
+++ main.c
@@ -247,19 +247,44 @@ void loadBars(){
     return;
 }
 
-void selectFromCategory(){
+void printSelectionCategory(char* type, MYSQL mysql){
+    MYSQL_RES *result = NULL;
+    MYSQL_ROW row;
+
+    char query[255];
+    char printer[255];
+    int count_row = 0;
+
+    strcpy(printer, "here's are all the ");
+    strcat(printer, type);
+    strcat(printer, " of the city of Paris :\n\n");
+    printf("%s\n", printer);
+
+    strcpy(query, "SELECT id, lieu1, date_periode FROM BAR WHERE libelle_type='");
+    strcat(query, type);
+    strcat(query, "'");
+    printf("%s\n", query);
+
+    mysql_query(&mysql,query);
+    result = mysql_store_result(&mysql);
+    while((row = mysql_fetch_row(result))){
+        printf("[%d] %s - %s\n",count_row+1,row[1],row[2]);
+        count_row++;
+    }
+
+}
+
+void selectFromCategory(MYSQL mysql){
     system("cls");
 
     int categorySelected;
-    char query[255];
-    char type[255];
-    MYSQL_RES *result=NULL;
-    MYSQL_ROW row;
+    int count_row = 0;
 
-    printf("Here you can find the type of place you want to go !\n\n");
+    printf("Here you can find the type of place you want to go !\n");
 
     do{
-        printf("Choose the type of place among what there is :\n\n");
+        count_row=0;
+        printf("\n\nChoose the type of place among what there is :\n\n");
         printf("1 - COMMERCE ACCESSOIRE\n\n");
         printf("2 - CONTRE ETALAGE\n\n");
         printf("3 - CONTRE TERRASSE\n\n");
@@ -275,30 +300,82 @@ void selectFromCategory(){
                 return;
                 break;
             case 1:
-                type = "COMMERCE ACCESSOIRE";
-                strcpy(query, "SELECT * FROM BAR WHERE libelle_type='");
-                strcat(query, type);
-                strcat(query, "'");
-                mysql_query(&mysql,query);
-                result = mysql_store_result(&mysql);
-                row = mysql_fetch_row(result);
-                if(row){
-
-                }
+                printSelectionCategory("COMMERCE ACCESSOIRE", mysql);
                 break;
             case 2:
-
+                printSelectionCategory("CONTRE ETALAGE", mysql);
+                break;
+            case 3:
+                printSelectionCategory("CONTRE TERRASSE", mysql);
+                break;
+            case 4:
+                printSelectionCategory("ETALAGE", mysql);
+                break;
+            case 5:
+                printSelectionCategory("PLANCHER MOBILE", mysql);
+                break;
+            case 6:
+                printSelectionCategory("TERRASSE FERMEE", mysql);
+                break;
+            case 7:
+                printSelectionCategory("TERRASSE OUVERTE", mysql);
                 break;
         }
     }while(1);
 
 }
 
-void selectMenu(){
+void researchFromAddress(MYSQL mysql){
+    system("cls");
+
+    MYSQL_RES *result = NULL;
+    MYSQL_ROW row;
+
+    char request[255];
+    char query[255];
+    int count_row = 0;
+    int choice;
+
+    printf("Here you can make a research from an entered address !\n\n");
+
+    do{
+        printf("\n\nPlease enter the street where you want to look for a bar :\n");
+        fflush(stdin);
+        fgets(request,255,stdin);
+        if(request[strlen(request)-1] == '\n'){
+               request[strlen(request)-1]='\0';
+           }
+        strcpy(query, "SELECT id, lieu1, date_periode, libelle_type FROM BAR WHERE (libelle_type = 'TERRASSE FERMEE' OR libelle_type = 'TERRASSE OUVERTE') AND lieu1 LIKE '%");
+        strcat(query, request);
+        strcat(query, "%'");
+        printf("\n");
+        mysql_query(&mysql,query);
+        result = mysql_store_result(&mysql);
+        while((row = mysql_fetch_row(result))){
+            printf("[%d] %s - %s - %s\n",count_row+1,row[1],row[2], row[3]);
+            count_row++;
+        }
+        printf("\n\nDo you want to make another research or get back ?\n\n");
+        printf("1 - Another research\n");
+        printf("0 - Get back\n");
+        scanf("%d", &choice);
+
+        switch(choice){
+            case 0:
+                return;
+                break;
+            case 1:
+                researchFromAddress(mysql);
+                break;
+        }
+
+    }while(1);
+
+}
+
+void selectMenu(MYSQL mysql){
 
     int menuSelection;
-
-    printf("Here you can find the bars you need !\n\n\n");
 
     do{
         system("cls");
@@ -306,20 +383,23 @@ void selectMenu(){
         printf("*** BARS ***\n\n");
         printf("1 - Load bar list (no need to do it everytime, your favorite bar will not disapear :D\n\n");
         printf("2 - Selection from a category\n\n");
-        printf("3 - make a research from an entry\n\n");
-        printf("0 - leave the program\n\n");
+        printf("3 - Make a research from an entry\n\n");
+        printf("0 - Leave the program\n\n");
         scanf("%d", &menuSelection);
 
         switch(menuSelection){
             case 0:
                 printf("\nEnjoy your drinks ;)\nSee you next time !\n");
-                return 0;
+                return;
                 break;
             case 1:
                 loadBars();
                 break;
             case 2:
-                selectFromCategory();
+                selectFromCategory(mysql);
+                break;
+            case 3:
+                researchFromAddress(mysql);
                 break;
         }
 
@@ -333,7 +413,14 @@ void selectMenu(){
 */
 int main(int argc, char **argv){
 
-    selectMenu();
+    MYSQL mysql;
+    mysql_init(&mysql);
+    mysql_options(&mysql,MYSQL_READ_DEFAULT_GROUP,"option");
+
+    if(mysql_real_connect(&mysql,"localhost","root","root","picomancer",0,NULL,0))
+        selectMenu(mysql);
+    else
+        printf("ERROR: An error occurred while connecting to the DB!");
 
     return 0;
 
